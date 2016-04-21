5290a294afc7110778a9edceab81dd31b57fce01
Christopher <cmullins@liveramp.com>
2016-04-21 00:06:38 -0700

    add root include

---
+++ lib/easy_upnp.rb
@@ -0,0 +1,6 @@
+require 'easy_upnp/ssdp_searcher'
+require 'easy_upnp/upnp_device'
+require 'easy_upnp/options_base'
+require 'easy_upnp/version'
+
+require 'easy_upnp/control_point/device_control_point'
